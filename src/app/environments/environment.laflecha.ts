29538ad82dc7f831af76daa35d3206bc2ab5b4d6
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2026-05-20 15:40:27 -0400

    fixes varios, deploy vercel

--- src/app/environments/environment.laflecha.ts
+++ src/app/environments/environment.laflecha.ts
@@ -1,15 +1,15 @@
 export const environment = {
     production: false,
-    url_backend: 'https://malcolmcordova.com/lawyer-app/backend-api-lawyer/',
-    url_servicios: 'https://malcolmcordova.com/lawyer-app/backend-api-lawyer/public/api',
+    url_backend: 'https://backend-laflecha-node.onrender.com/',
+    url_servicios: 'https://backend-laflecha-node.onrender.com/api',
     url_frontend: 'https://malcolmcordova.com/lawyer-app/',
     mediaUrlRemoto: 'https://res.cloudinary.com/dmv6aukai/image/upload/v1741218430/lawyerapp',
   soketServer : 'http://localhost:3000',
     //paypal
-    clientSubscripcionesSandboxId: 'Aebb_SJ2-L8OrgRAvAERINfyMb7eKrqZ7xPt5JreBd9eYfDjzfDildStuo5Gjcx6GNvWbTZaiwBiMeAf',
+  clientSubscripcionesSandboxId: 'ASaZPL8SA8etEH2nd6PDl5B_etUSC66IbS8Ii2Pnz9-_hO09g-1OrCyeyIkZaQLaE0PKpkrxYKVAH7si',
 
   //notificaciones
-  urlBackedNotification:'https://backend-condoparquecentral-mean.onrender.com/api/notipush/save-subscription',
+  urlBackedNotification:'https://backend-laflecha-node.onrender.com/api/notipush/save-subscription',
   VAPI_KEY_PUBLIC: 'BNc3MfntrpdWAMI0usltUAh-w1RRvkbD_8QdjZRHPCpLVlCxIm2ZQ4N1w_iX49RUAMNR11Valdl8DlIH6PiYa9U',
  
     
--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -6,10 +6,10 @@ export const environment = {
   mediaUrlRemoto: 'https://res.cloudinary.com/dmv6aukai/image/upload/v1741218430/lawyerapp',
   soketServer : 'http://localhost:3000',
   //paypal
-    clientSubscripcionesSandboxId: 'Aebb_SJ2-L8OrgRAvAERINfyMb7eKrqZ7xPt5JreBd9eYfDjzfDildStuo5Gjcx6GNvWbTZaiwBiMeAf',
+  clientSubscripcionesSandboxId: 'ASaZPL8SA8etEH2nd6PDl5B_etUSC66IbS8Ii2Pnz9-_hO09g-1OrCyeyIkZaQLaE0PKpkrxYKVAH7si',
 
   //notificaciones
-  urlBackedNotification:'https://backend-condoparquecentral-mean.onrender.com/api/notipush/save-subscription',
+  urlBackedNotification:'https://backend-laflecha-node.onrender.com/api/notipush/save-subscription',
   VAPI_KEY_PUBLIC: 'BNc3MfntrpdWAMI0usltUAh-w1RRvkbD_8QdjZRHPCpLVlCxIm2ZQ4N1w_iX49RUAMNR11Valdl8DlIH6PiYa9U',
  
 };
